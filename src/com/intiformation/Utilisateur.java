4b7fdd99ae5346a04965cbb1ed489744309eb2de
kevin wanaverbecq <kevin-wanaverbecq@hotmail.com>
2020-07-02 11:29:05 +0200

    ajout des props de la classe Utilisateur

--- src/com/intiformation/Utilisateur.java
+++ src/com/intiformation/Utilisateur.java
@@ -1,5 +1,15 @@
 package com.intiformation;
 
+/**
+ * 
+ * @author Kevin
+ *
+ */
 public class Utilisateur {
 
-}
+	/*__________ props __________*/
+	private int id;
+	private String nom;
+	private String prenom;
+	
+}//end class
