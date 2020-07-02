77c4fb27f7f3d4860cd05d89223c561df32d4bcd
kevin wanaverbecq <kevin-wanaverbecq@hotmail.com>
2020-07-02 11:34:13 +0200

    ajout des ctors à la classe Utilisateur

--- src/com/intiformation/Utilisateur.java
+++ src/com/intiformation/Utilisateur.java
@@ -12,4 +12,27 @@ public class Utilisateur {
 	private String nom;
 	private String prenom;
 	
+	/*__________ ctors __________*/
+	/**
+	 * ctor vide
+	 */
+	public Utilisateur() {
+		super();
+	}
+	
+	/**
+	 * ctor chargé
+	 * @param id
+	 * @param nom
+	 * @param prenom
+	 */
+	public Utilisateur(int id, String nom, String prenom) {
+		super();
+		this.id = id;
+		this.nom = nom;
+		this.prenom = prenom;
+	}
+	
+	
+	
 }//end class
